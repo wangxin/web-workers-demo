c9c3a15c8f2d111ecd30efa71c97976fdfcb260d
Xin Wang <wangxin.wang@gmail.com>
2017-06-12 18:09:14 +0800

    Add worker to process image

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -5,6 +5,7 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+  var worker = new Worker('scripts/worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -42,22 +43,14 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
+    worker.postMessage({imageData: imageData, type: type});
+  };
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
+  worker.addEventListener('message', function (e) {
+    imageData = e.data;
     toggleButtonsAbledness();
     return ctx.putImageData(imageData, 0, 0);
-  };
+  });
 
   function revertImage() {
     return ctx.putImageData(original, 0, 0);
